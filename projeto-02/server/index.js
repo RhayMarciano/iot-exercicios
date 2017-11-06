500fa38613e7188b72bba8f32dc25d1913fa3700
Paolo Bueno <paolohaji@gmail.com>
2017-11-06 18:56:20 -0200

    Adicionar persistencia de memoria para mosca

--- projeto-02/server/index.js
+++ projeto-02/server/index.js
@@ -1,16 +1,14 @@
 const mosca = require('mosca');
-const path = require('path');
 
 const mqttServer = new mosca.Server({
   port: 1883,
   http: {
     port: 8080,
     static: './public/'
+  },
+  persistence: {
+    factory: mosca.persistence.Memory
   }
 });
 
 mqttServer.on('ready', () => console.log('server ready'));
-mqttServer.on('published', (packet, client) => {
-  console.log('publish', packet);
-  console.log('client', client);
-});
