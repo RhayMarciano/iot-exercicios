e6a38221e0f7c6c3c4f92b1b55a52509dfaea0b0
Paolo Bueno <paolohaji@gmail.com>
2017-11-08 18:27:16 -0200

    Adicionar display do endereco do server

--- projeto-02/server/public/index.js
+++ projeto-02/server/public/index.js
@@ -1,6 +1,9 @@
 /*global Vue*/
 /*global mqtt*/
+const url = new URL(window.location.href);
 const data = {
+  href: url.toString(),
+  url: url.host,
   count: 0,
   topics: {
   }
@@ -11,10 +14,9 @@ new Vue({
   data
 });
 
-const url = new URL(window.location.href);
 url.protocol = 'ws://';
 const brokerUrl = url.toString();
-const topicPrefix = 'senai-code-xp-vagas/';
+const topicPrefix = 'vagas/';
 console.log('attempting connection');
 var client = mqtt.connect(brokerUrl);
 
@@ -39,7 +41,7 @@ client.on('message', (topic, message) => console.info(topic, message.toString())
 client.on('message', (topic, message) => {
   const msg = message.toString();
   if (msg === '') {
-    Vue.delete(data.topics, topic, msg)
+    Vue.delete(data.topics, topic, msg);
   } else {
     Vue.set(data.topics, topic, msg);
   }
